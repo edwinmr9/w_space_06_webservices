ce4f09ed7e683ebd244fbaab6a6ea7ee54ee9865
LAPTOP\Host09 <edwmr9@gmail.com>
2022-09-10 21:47:59 +0200

    se agrego el 41_other_drill

---
+++ 41_other_drill_mcr_01/src/main/java/controller/SaludoController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SaludoController {
+	@GetMapping(value="/saludo", produces=MediaType.TEXT_PLAIN_VALUE)
+	public String saludar() {
+		return"======== WELCOME TO MY SPRING BOOT ==========="; 
+	}
+
+@GetMapping(value="/contar/{cad}", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String contar (@PathVariable("cad") String cad) {
+		return String.valueOf(cad.length()); 
+	}
+}
